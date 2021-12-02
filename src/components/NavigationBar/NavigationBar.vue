76a989e162f604f609409beac84bd12a2aedd0f9
Jakob Bach Nielsen <jn@lait.dk>
2021-12-02 12:04:13 +0100

    Add fonts and font face

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -1,10 +1,18 @@
 <template>
 	<header>
 		<nav class="nav-bar">
-			<div class="nav-bar__logo">logo</div>
-			<template v-for="(link, index) in navLinks" :key="index">
-				<p>{{ link.text }}</p>
-			</template>
+			<div class="nav-bar__logo flex flex-center-center">logo</div>
+			<ul class="nav-list flex flex-stacked">
+				<li
+					v-for="(link, index) in navLinks"
+					:key="index"
+					class="nav-list__item flex flex-stacked flex-center-center"
+					:class="{ active: !index }"
+				>
+					<div>logo</div>
+					<p>{{ link.text }}</p>
+				</li>
+			</ul>
 		</nav>
 	</header>
 </template>
